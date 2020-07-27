5e15ed40cb38c9159a661e4f5d9b90cd168a3c3c
malinmaria <malin.nemergut@gmail.com>
2020-07-26 21:59:14 -0400

    finished cosmetics req form test

--- PennCosmeticsLive/Cosmetics Pages/test_consultRequestForm.py
+++ PennCosmeticsLive/Cosmetics Pages/test_consultRequestForm.py
@@ -1,5 +1,7 @@
 import unittest
+import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 class MyTestCase(unittest.TestCase):
@@ -9,8 +11,26 @@ class MyTestCase(unittest.TestCase):
         self.driver.get('https://www.pennmedicine.org/cosmetic-services/request-cosmetic-services-consult')
 
     def test_populateForm(self):
-        self.driver.find_element_by_id('tfa_52-L').send_keys("Test First Name")
-        
+        time.sleep(3)
+        iframe = self.driver.find_element_by_id("pcmaincontent_0_ctl02_divFormContainer").find_element_by_tag_name('iframe')
+        self.driver.switch_to.frame(iframe)
+        self.driver.implicitly_wait(10)
+        self.driver.find_element(By.ID, "tfa_52").send_keys("Test First Name")
+        self.driver.find_element(By.ID, "tfa_53").send_keys("Test Last Name")
+        dob = self.driver.find_element(By.ID, "tfa_54")
+        dob.click()
+        dob.send_keys("01011999")
+        phone = self.driver.find_element(By.ID, "tfa_55")
+        phone.click()
+        phone.send_keys("1231231234")
+        self.driver.find_element(By.ID, "tfa_56").send_keys("21211")
+        self.driver.find_element(By.ID, "tfa_57").send_keys("test@mail.com")
+        self.driver.find_element(By.ID, "tfa_72").send_keys("Typing in a test reason")
+        #self.driver.find_element_by_class_name("input-checkbox-faux").click()
+        self.driver.find_element_by_id("submit_button").click()
+        time.sleep(3)
+        assert self.driver.find_element(By.CSS_SELECTOR, "b").text == "Thank you for your request"
+
 
     def tearDown(self):
         self.driver.quit()
